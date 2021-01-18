7435198f2863a543afd98b19f0f66fc839c61ba6
Pgielly <p.gielly@it-students.com>
2021-01-18 11:14:21 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Acceuil - Blog</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Acceuil</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
